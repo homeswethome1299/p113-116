5f115a61de594f94a6db9dfc618f877aca4f5048
homeswethome1299 <120109907+homeswethome1299@users.noreply.github.com>
2024-01-05 17:49:28 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(700,500);
+    canvas.center();
+}
+
+function draw()
+{
+
+}
+function take_snapshot()
+{
+    save("meh.png");
+}
